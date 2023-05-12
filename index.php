a9df78e5fabf2bea1302819a01dd065a43a5bd05
Isra Rafi <israrafi0317@gmail.com>
2023-05-12 12:58:58 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Isra Rafi.com</title>
+</head>
+
+<body style="background:darkgray;">
+    <div style="color:rgb(252, 252, 252); padding: 30px; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
+        <h1 style="color: blue;">Selamat Datang di isra rafi sidqi channel</h1>
+        <h3>Demonstrasi Google App Engine</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dignissimos praesentium reprehenderit quia adipisci aliquid sapiente numquam esse? Dolore nam provident beatae fugit consequatur illum optio necessitatibus quas? Molestiae, delectus.</p>
+    </div>
+</body>
+
+</html>
